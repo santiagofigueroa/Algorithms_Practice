c79f6d75acb3796cb5f4453420d387e0263d388f
Santiago <santiago_figueroa_@hotmail.com>
2021-02-15 10:43:30 +0000

    All added except for bubble sort

---
+++ DataStructures/DataStructures/BubbleSort.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataStructures
+{
+	public static class BubbleSort
+	{
+
+		public static int[] bubblesort(int [] arr,  int n )
+		{	
+			for (int pass = n -1; pass >= 0;  pass--)
+			{
+				for (int i = 0; i <= pass -1; i++)
+				{
+					if(arr[i] > arr[i + 1])
+					{
+						var temp = arr[i];
+						arr[i] = arr[i + 1];
+						arr[i + 1] = temp;
+						Console.WriteLine(arr[i]);
+					}
+				}	
+
+
+			}
+			return arr;
+
+		} 
+
+	}
+}
